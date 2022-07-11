39b8330a78d2a7dd82bdaf7d0826f8ebe427256a
hasan-naim <hasannaim6246@gmail.com>
2022-07-11 19:16:58 +0600

    search is now works

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchImg from '../assets/icons/search.svg';
 import TrendingDiv from './TrendingDiv';
 
@@ -8,6 +8,20 @@ function Home() {
 
     // state for check clicking in the input feild
     const [showTrendingDiv, setShowTrendingDiv] = useState(false);
+
+    const [searchData, setSearchData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch(
+                `https://api.themoviedb.org/3/search/movie?api_key=8192cc786ef61b56059ee40953b95ac1&language=en-US&query=${text}&page=1&include_adult=false`
+            );
+            const data = await res.json();
+            setSearchData(data.results.filter((movie) => movie.poster_path !== null));
+        };
+        fetchData();
+    }, [text]);
+
     return (
         <section className={`home ${!text && `img`}`}>
             <div className="container">
@@ -25,7 +39,20 @@ function Home() {
                 {showTrendingDiv && !text && <TrendingDiv />}
                 {text && (
                     <div className="search-div">
-                        <h1>searhing:</h1>
+                        <h1>Search Result: {text}</h1>
+                        {searchData?.map((data) => (
+                            <div className="wrapper">
+                                <div className="wrapper-width">
+                                    <div className="search-card">
+                                        <img
+                                            src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+                                            alt=""
+                                        />
+                                        <span className="title"> {data.title}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 )}
             </div>
