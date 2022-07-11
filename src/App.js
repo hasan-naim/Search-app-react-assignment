d8ed70650173d34f14a71773c47a5c6adb8dd794
hasan-naim <hasannaim6246@gmail.com>
2022-07-11 13:28:54 +0600

    search input component is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.scss';
+import Layout from './Components/Layout';
+import './styles/App.scss';
 
 function App() {
     return (
         <div className="App">
-            <h1>hello</h1>
+            <Layout />
         </div>
     );
 }
---
+++ src/Components/Layout.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SearchInput from './SearchInput';
+
+function Layout() {
+    return <SearchInput />;
+}
+
+export default Layout;
